67ca6ebd86c1f6a75ff1020aa398ab819dade25b
herobrauni <braunnicolaj@gmail.com>
2024-12-11 16:48:52 +0100

    whatever

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,13 +8,13 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        \App\Models\User::factory(1)->create();//Disable demo first user creation
+        // \App\Models\User::factory(1)->create();//Disable demo first user creation
         $this->call(SettingsSeeder::class);
         $this->call(ProvidersSeeder::class);
         $this->call(LocationsSeeder::class);
         $this->call(OsSeeder::class);
         $this->call(LabelsSeeder::class);
-       //$this->call(DomainsSeeder::class);
+        //$this->call(DomainsSeeder::class);
         //$this->call(ServersSeeder::class);
         //$this->call(SharedSeeder::class);
     }
